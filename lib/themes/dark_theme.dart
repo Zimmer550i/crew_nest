60ab73f6ea746ac58801e375f310c2d7e81c321d
Wasiul Islam Sakif <sakif049@gmail.com>
2026-07-09 10:08:23 +0600

    init: initialize repo

---
+++ lib/themes/dark_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:crew_nest/utils/app_colors.dart';
+
+ThemeData dark() => ThemeData(
+  fontFamily: "Lato",
+  brightness: Brightness.dark,
+  scaffoldBackgroundColor: AppColors.gray.shade900,
+  colorScheme: ColorScheme.dark(
+    primary: AppColors.green,
+    secondary: AppColors.green.shade300,
+    surface: AppColors.gray.shade800,
+  ),
+);
---
+++ lib/views/screens/design_system.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:crew_nest/views/widgets/custom_app_bar.dart';
+import 'package:crew_nest/views/widgets/custom_button.dart';
+import 'package:crew_nest/views/widgets/custom_text_field.dart';
+import 'package:crew_nest/views/widgets/profile_picture.dart';
+
+class DesignSystem extends StatelessWidget {
+  const DesignSystem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(title: "Design System"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          spacing: 16,
+          children: [
+            ProfilePicture(
+              image: "https://picsum.photos/500/500",
+              isEditable: true,
+            ),
+            CustomTextField(title: "Email", hintText: "Enter your email"),
+            CustomTextField(
+              title: "Password",
+              hintText: "Enter your password",
+              isPassword: true,
+            ),
+            CustomButton(text: "Button"),
+          ],
+        ),
+      ),
+    );
+  }
+}
